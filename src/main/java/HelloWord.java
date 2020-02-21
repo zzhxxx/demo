0dc1030f28a95a962dc30a091dc8ca911f1a8d62
赵紫辉 <justdosentry@163.com>
2020-02-21 12:00:45 +0800

    测试冲突

--- src/main/java/HelloWord.java
+++ src/main/java/HelloWord.java
@@ -2,7 +2,7 @@ public class HelloWord {
 
 
     public static void main(String[] args) {
-        System.out.println("hello word");
+        System.out.println("hello word123123");
         System.out.println(11333);
         System.out.println("2222");
     }
