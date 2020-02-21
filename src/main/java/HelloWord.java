8aa0a49cb98b1cea2c703339aa8a536179cfe81c
ziyi <729410980@qq.com>
2020-02-21 11:46:19 +0800

    222

--- src/main/java/HelloWord.java
+++ src/main/java/HelloWord.java
@@ -4,5 +4,6 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println(1111);
+        System.out.println("2222");
     }
 }
