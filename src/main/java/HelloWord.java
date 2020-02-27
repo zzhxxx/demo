7e436beef1b766b47ce2fe8ab544397c311b20b6
ziyi <729410980@qq.com>
2020-02-27 15:57:44 +0800

    zzz1加代码

--- src/main/java/HelloWord.java
+++ src/main/java/HelloWord.java
@@ -3,7 +3,7 @@ public class HelloWord {
 
     public static void main(String[] args) {
         System.out.println("hello word hello scala");
-
+        System.out.println(434);
         System.out.println(11333);
         System.out.println("");
 
